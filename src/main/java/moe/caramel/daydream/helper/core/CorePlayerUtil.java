3548333e63d044acf2c879d263be6849132f1891
LemonCaramel <admin@caramel.moe>
2023-05-19 03:58:41 +0900

    add: CorePlayerUtil#sendLoginPacket

--- src/main/java/moe/caramel/daydream/helper/core/CorePlayerUtil.java
+++ src/main/java/moe/caramel/daydream/helper/core/CorePlayerUtil.java
@@ -2,17 +2,25 @@ package moe.caramel.daydream.helper.core;
 
 import io.papermc.paper.adventure.PaperAdventure;
 import net.kyori.adventure.text.Component;
+import net.minecraft.core.RegistryAccess;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientGamePacketListener;
 import net.minecraft.network.protocol.game.ClientboundBundlePacket;
+import net.minecraft.network.protocol.game.ClientboundLoginPacket;
 import net.minecraft.network.protocol.game.ClientboundRemoveEntitiesPacket;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ChunkMap;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.players.PlayerList;
+import net.minecraft.world.level.GameRules;
+import net.minecraft.world.level.biome.BiomeManager;
 import org.bukkit.craftbukkit.v1_19_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -132,6 +140,48 @@ public final class CorePlayerUtil {
     public static void playerListNameNonUpdate(@NotNull Player player, @Nullable Component name) {
         final ServerPlayer sPlayer = ((CraftPlayer) player).getHandle();
         sPlayer.listName = (name == null) ? null : PaperAdventure.asVanilla(name);
+    }
+
+    // ================================
+
+    /**
+     * 플레이어에게 로그인 패킷을 전송합니다.
+     * 패킷 유틸리티가 월드 NBT에 접근해야 하는 경우 유용합니다.
+     *
+     * @param player 대상 플레이어
+     * @param maxPlayers 최대 플레이어 수 (매직밸류로 가짜 패킷을 구분)
+     */
+    public static void sendLoginPacket(@NotNull Player player, final int maxPlayers) throws IllegalAccessException {
+        final ServerPlayer sPlayer = ((CraftPlayer) player).getHandle();
+        final ServerLevel sLevel = sPlayer.getLevel();
+        final GameRules rules = sLevel.getGameRules();
+        final MinecraftServer server = sLevel.getServer();
+
+        // Get Synchronized Registries
+        RegistryAccess.Frozen synchronizedRegistries = null;
+        for (final Field field : PlayerList.class.getDeclaredFields()) {
+            if (field.getDeclaringClass() == RegistryAccess.Frozen.class) {
+                synchronizedRegistries = (RegistryAccess.Frozen) field.get(server.getPlayerList());
+            }
+        }
+
+        if (synchronizedRegistries == null) {
+            throw new NullPointerException();
+        }
+
+        // Send Packet
+        sPlayer.connection.send(new ClientboundLoginPacket(
+            sPlayer.getId(), sLevel.getLevelData().isHardcore(),
+            sPlayer.gameMode.getGameModeForPlayer(), sPlayer.gameMode.getPreviousGameModeForPlayer(),
+            server.levelKeys(), synchronizedRegistries, sLevel.dimensionTypeId(), sLevel.dimension(),
+            BiomeManager.obfuscateSeed(sLevel.getSeed()), maxPlayers,
+            sLevel.getChunkSource().chunkMap.playerChunkManager.getTargetSendDistance(),
+            sLevel.getChunkSource().chunkMap.playerChunkManager.getTargetTickViewDistance(),
+            rules.getBoolean(GameRules.RULE_REDUCEDDEBUGINFO),
+            !rules.getBoolean(GameRules.RULE_DO_IMMEDIATE_RESPAWN),
+            sLevel.isDebug(), sLevel.isFlat(),
+            sPlayer.getLastDeathLocation()
+        ));
     }
 
     // ================================
